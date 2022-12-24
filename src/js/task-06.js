77c533ffa63eb64bfa471d79151087708c99704f
Natalia Moskvychova <110030591+NaTaLiaMoSKV@users.noreply.github.com>
2022-12-24 12:31:25 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,12 +4,12 @@ inputEl.addEventListener('blur', onInputElBlur);
 function onInputElBlur(event) {
     const input = event.currentTarget;
     if(input.value.length == input.dataset.length) {
-        input.classList.add('.valid');
-        input.classList.remove('.invalid');
+        input.classList.add('valid');
+        input.classList.remove('invalid');
         console.log(input);
     } else {
-        input.classList.add('.invalid');
-        input.classList.remove('.valid');
+        input.classList.add('invalid');
+        input.classList.remove('valid');
         console.log(input);
     }
 }
